e77102a961936579a44d1c3d0b0b4f96ff0cf1ba
euuLuizCarlos <euuluizcarlos@gmail.com>
2025-08-18 22:32:11 -0300

    funcionou amem

--- routes/livrosRoutes.js
+++ routes/livrosRoutes.js
@@ -5,5 +5,9 @@ const livrosController =
   require('../controllers/livrosController');
 
 route.get('/', livrosController.listar);
+route.get('/criar', livrosController.criar);
+route.get('/:id/editar', livrosController.editar);
+route.post('/', livrosController.inserir);
+route.post('/:id', livrosController.atualizar);
 
 module.exports = route;
